b223f9e7db2bfd6d21e50c4d641b79e304b50ef9
David Carpenter <chipmunksvideos@gmail.com>
2012-02-23 18:42:36 -0800

    Using the new Vanish api

--- src/com/Chipmunk9998/Spectate/Spectate.java
+++ src/com/Chipmunk9998/Spectate/Spectate.java
@@ -1,97 +1,70 @@
 package com.Chipmunk9998.Spectate;
 
-import net.minecraft.server.EntityPlayer;
-import net.minecraft.server.Packet29DestroyEntity;
-
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.craftbukkit.entity.CraftEntity;
-import org.bukkit.craftbukkit.entity.CraftPlayer;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import net.minecraft.server.Packet20NamedEntitySpawn;
-
 public class Spectate extends JavaPlugin {
-	
+
 	FileConfiguration conf;
-	
+
 	private final SpectateListener Listener = new SpectateListener(this);
 	final SpectateCommandExecutor CommandExecutor = new SpectateCommandExecutor(this);
-	  
+
 	public void onDisable() {
-		
+
 		for (Player players : getServer().getOnlinePlayers()) {
-			
+
 			if (CommandExecutor.isSpectating.get(players) != null) {
-				
+
 				if (CommandExecutor.isSpectating.get(players)) {
-					
+
 					players.sendMessage("§7You were forced to stop spectating because of a server reload.");
-					
+
 					players.teleport(CommandExecutor.origLocation.get(players));
 					CommandExecutor.isSpectating.put(players, false);
 					CommandExecutor.isBeingSpectated.put(CommandExecutor.target.get(players), false);
-					
+					players.getInventory().clear();
+					players.getInventory().setContents(CommandExecutor.senderInv.get(players));
+					players.getInventory().setArmorContents(CommandExecutor.senderArm.get(players));
+					players.setHealth(CommandExecutor.senderHealth.get(players));
+					players.getPlayer().setFoodLevel(CommandExecutor.senderHunger.get(players));
+
 					for (Player playp : players.getWorld().getPlayers()) {
-					
-						visible(players, playp);
-						
+
+						players.showPlayer(playp);
+
 					}
-					
-					getNative(players).netServerHandler.sendPacket(new Packet29DestroyEntity(players.getEntityId()));
-					visible(CommandExecutor.target.get(players), players);
-					
+
 				}
-				
+
 			}
-			
+
 		}
-		
+
 		System.out.println("Spectate is disabled!");
-	    
+
 	}
-	  
+
 	public void onEnable() {
 
-		  getServer().getPluginManager().registerEvents(Listener, this);
-		  
-		  PluginDescriptionFile pdfFile = this.getDescription();
-		    
-		  System.out.println(pdfFile.getName() + " v" + pdfFile.getVersion() + " enabled!");
-		    
-		  conf = getConfig();
-			  
-		  if (conf.get("Enable Permissions?") == null) {
-				
-			conf.set("Enable Permissions?", false);
+		getServer().getPluginManager().registerEvents(Listener, this);
+
+		PluginDescriptionFile pdfFile = this.getDescription();
+
+		System.out.println(pdfFile.getName() + " v" + pdfFile.getVersion() + " enabled!");
+
+		conf = getConfig();
+
+		if (conf.get("Enable Permissions?") == null) {
+
+			conf.set("Enable Permissions?", true);
 			saveConfig();
-				
-		  }
-		  
-		  getCommand("spectate").setExecutor(CommandExecutor);
-		  getCommand("spectateoff").setExecutor(CommandExecutor);
+
+		}
+
+		getCommand("spectate").setExecutor(CommandExecutor);
+		getCommand("spectateoff").setExecutor(CommandExecutor);
 	}
-	  
-	  public static net.minecraft.server.Entity getNative(Entity e) {
-		  
-	        return ((CraftEntity) e).getHandle();
-	        
-	    }
-	  
-	    public EntityPlayer getNative(Player p) {
-	    	
-	        return ((EntityPlayer) getNative((Entity) p));
-	        
-	    }
-	    
-	    public void visible(Player p1, Player p2) {
-	    	
-	    	CraftPlayer unHide = (CraftPlayer) p1;
-	    	CraftPlayer unHideFrom = (CraftPlayer) p2;
-	    	
-	    	unHideFrom.getHandle().netServerHandler.sendPacket(new Packet20NamedEntitySpawn(unHide.getHandle()));
-	    	
-	    }
 }
--- src/com/Chipmunk9998/Spectate/SpectateListener.java
+++ src/com/Chipmunk9998/Spectate/SpectateListener.java
@@ -1,6 +1,5 @@
 package com.Chipmunk9998.Spectate;
 
-import net.minecraft.server.Packet29DestroyEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -17,174 +16,202 @@ import org.bukkit.event.player.PlayerQuitEvent;
 public class SpectateListener implements Listener {
 
 	public Spectate plugin;
-	
+
 	int varx;
 	int vayr;
 	int varz;
-	
+
 	public SpectateListener(Spectate plugin) {
 
 		this.plugin = plugin;
 
 	}
-	
+
 	@EventHandler
 	public void onPlayerMove(PlayerMoveEvent event) {
-		    	
-		    	if (plugin.CommandExecutor.isBeingSpectated.get(event.getPlayer()) != null) {
-		    		
-		    		if (plugin.CommandExecutor.isBeingSpectated.get(event.getPlayer())) {
-		    	
-		    			plugin.CommandExecutor.spectator.get(event.getPlayer()).getPlayer().teleport(plugin.CommandExecutor.target.get(plugin.CommandExecutor.spectator.get(event.getPlayer())));
-		    			
-		    			plugin.CommandExecutor.spectator.get(event.getPlayer()).getInventory().clear();
-		    			plugin.CommandExecutor.spectator.get(event.getPlayer()).getInventory().setContents(event.getPlayer().getInventory().getContents());
-		    			plugin.CommandExecutor.spectator.get(event.getPlayer()).getInventory().setArmorContents(event.getPlayer().getInventory().getArmorContents());
-		    			
-		    			for (Player p : plugin.CommandExecutor.spectator.get(event.getPlayer()).getWorld().getPlayers()) {
-    		    			
-            				plugin.getNative(p).netServerHandler.sendPacket(new Packet29DestroyEntity(plugin.CommandExecutor.spectator.get(event.getPlayer()).getEntityId()));
-            		                
-            			}
-            		    		
-            		    	plugin.getNative(plugin.CommandExecutor.spectator.get(event.getPlayer())).netServerHandler.sendPacket(new Packet29DestroyEntity(event.getPlayer().getEntityId()));
-		    		
-		    		}
-		    		
-		    	}else {
-		    		
-		    		if (plugin.CommandExecutor.isSpectating.get(event.getPlayer()) != null) {
-		    			
-		    			if (plugin.CommandExecutor.isSpectating.get(event.getPlayer())) {
-		    				
-		    				event.getPlayer().teleport(plugin.CommandExecutor.target.get(event.getPlayer()));
-		    				
-		    				for (Player p : event.getPlayer().getWorld().getPlayers()) {
-	    		    			
-	            				plugin.getNative(p).netServerHandler.sendPacket(new Packet29DestroyEntity(event.getPlayer().getEntityId()));
-	            		                
-	            			}
-	            		    		
-	            		    	plugin.getNative(event.getPlayer()).netServerHandler.sendPacket(new Packet29DestroyEntity(plugin.CommandExecutor.target.get(event.getPlayer()).getEntityId()));
-		    				
-		    			}
-		    			
-		    		}
-		    		
-		    	}
-		
+
+		if (plugin.CommandExecutor.isBeingSpectated.get(event.getPlayer()) != null) {
+
+			if (plugin.CommandExecutor.isBeingSpectated.get(event.getPlayer())) {
+
+				plugin.CommandExecutor.spectator.get(event.getPlayer()).getPlayer().teleport(plugin.CommandExecutor.target.get(plugin.CommandExecutor.spectator.get(event.getPlayer())));
+
+				plugin.CommandExecutor.spectator.get(event.getPlayer()).getInventory().clear();
+				plugin.CommandExecutor.spectator.get(event.getPlayer()).getInventory().setContents(event.getPlayer().getInventory().getContents());
+				plugin.CommandExecutor.spectator.get(event.getPlayer()).getInventory().setArmorContents(event.getPlayer().getInventory().getArmorContents());
+
+				if (event.getPlayer().getHealth() > 0) {
+
+					plugin.CommandExecutor.spectator.get(event.getPlayer()).setHealth(event.getPlayer().getHealth());
+
+				}else {
+
+					plugin.CommandExecutor.spectator.get(event.getPlayer()).setHealth(1);
+
+				}
+
+				if (event.getPlayer().isSprinting()) {
+
+					plugin.CommandExecutor.spectator.get(event.getPlayer()).setSprinting(true);
+
+				}else {
+
+					plugin.CommandExecutor.spectator.get(event.getPlayer()).setSprinting(false);
+
+				}
+
+				for (Player p : plugin.CommandExecutor.spectator.get(event.getPlayer()).getWorld().getPlayers()) {
+
+					plugin.CommandExecutor.spectator.get(event.getPlayer()).hidePlayer(p);
+
+				}
+
+				event.getPlayer().hidePlayer(plugin.CommandExecutor.spectator.get(event.getPlayer()));
+
+			}
+
+		}
+
+		if (plugin.CommandExecutor.isSpectating.get(event.getPlayer()) != null) {
+
+			if (plugin.CommandExecutor.isSpectating.get(event.getPlayer())) {
+
+				event.getPlayer().teleport(plugin.CommandExecutor.target.get(event.getPlayer()));
+
+				for (Player p : event.getPlayer().getWorld().getPlayers()) {
+
+					event.getPlayer().hidePlayer(p);
+
+				}
+
+				plugin.CommandExecutor.target.get(event.getPlayer()).hidePlayer(event.getPlayer());
+
+			}
+
+		}
+
 	}
-	
+
 	@EventHandler
 	public void onPlayerQuit(PlayerQuitEvent event) {
-		
+
 		if (plugin.CommandExecutor.isSpectating.get(event.getPlayer()) != null) {
-		
+
 			if (plugin.CommandExecutor.isSpectating.get(event.getPlayer())) {
-				
+
+				event.getPlayer().setFoodLevel(plugin.CommandExecutor.target.get(event.getPlayer()).getFoodLevel());
+
 				event.getPlayer().teleport(plugin.CommandExecutor.origLocation.get(event.getPlayer()));
 				plugin.CommandExecutor.isSpectating.put(event.getPlayer(), false);
-				
+				event.getPlayer().getInventory().clear();
+				event.getPlayer().getInventory().setContents(plugin.CommandExecutor.senderInv.get(event.getPlayer()));
+				event.getPlayer().getInventory().setArmorContents(plugin.CommandExecutor.senderArm.get(event.getPlayer()));
+				event.getPlayer().setHealth(plugin.CommandExecutor.senderHealth.get(event.getPlayer()));
+				event.getPlayer().setFoodLevel(plugin.CommandExecutor.senderHunger.get(event.getPlayer()));
+
 			}
 		}
-		
+
 		if (plugin.CommandExecutor.isBeingSpectated.get(event.getPlayer()) != null) {
-			
+
 			if (plugin.CommandExecutor.isBeingSpectated.get(event.getPlayer())) {
-				
+
 				plugin.CommandExecutor.spectator.get(event.getPlayer()).sendMessage("§7You were forced to stop spectating because the person who you were spectating disconnected.");
-				
+
 				plugin.CommandExecutor.spectator.get(event.getPlayer()).teleport(plugin.CommandExecutor.origLocation.get(plugin.CommandExecutor.spectator.get(event.getPlayer())));
 				plugin.CommandExecutor.isSpectating.put(plugin.CommandExecutor.spectator.get(event.getPlayer()), false);
 				plugin.CommandExecutor.isBeingSpectated.put(event.getPlayer(), false);
-				
+				plugin.CommandExecutor.spectator.get(event.getPlayer()).getInventory().clear();
+				plugin.CommandExecutor.spectator.get(event.getPlayer()).getInventory().setContents(plugin.CommandExecutor.senderInv.get(plugin.CommandExecutor.spectator.get(event.getPlayer())));
+				plugin.CommandExecutor.spectator.get(event.getPlayer()).getInventory().setArmorContents(plugin.CommandExecutor.senderArm.get(plugin.CommandExecutor.spectator.get(event.getPlayer())));
+				plugin.CommandExecutor.spectator.get(event.getPlayer()).setHealth(plugin.CommandExecutor.senderHealth.get(plugin.CommandExecutor.spectator.get(event.getPlayer())));
+				plugin.CommandExecutor.spectator.get(event.getPlayer()).setFoodLevel(plugin.CommandExecutor.senderHunger.get(plugin.CommandExecutor.spectator.get(event.getPlayer())));
+
 				for (Player playp : plugin.CommandExecutor.spectator.get(event.getPlayer()).getWorld().getPlayers()) {
-				
-					plugin.visible(plugin.CommandExecutor.spectator.get(event.getPlayer()), playp);
-					
+
+					plugin.CommandExecutor.spectator.get(event.getPlayer()).showPlayer(playp);
+
 				}
-				
-				plugin.getNative(plugin.CommandExecutor.spectator.get(event.getPlayer())).netServerHandler.sendPacket(new Packet29DestroyEntity(plugin.CommandExecutor.spectator.get(event.getPlayer()).getEntityId()));
-				plugin.visible(plugin.CommandExecutor.target.get(plugin.CommandExecutor.spectator.get(event.getPlayer())), plugin.CommandExecutor.spectator.get(event.getPlayer()));
-				
+
 			}
-			
+
 		}
+
 	}
-	
+
 	@EventHandler
 	public void onPlayerPickupItem(PlayerPickupItemEvent event) {
-		
+
 		if (plugin.CommandExecutor.isSpectating.get(event.getPlayer()) != null) {
-			
+
 			if (plugin.CommandExecutor.isSpectating.get(event.getPlayer())) {
-				
+
 				event.setCancelled(true);
-				
+
 			}
-			
+
 		}
-		
+
 	}
-	
+
 	@EventHandler
 	public void onPlayerDropItem(PlayerDropItemEvent event) {
-		
+
 		if (plugin.CommandExecutor.isSpectating.get(event.getPlayer()) != null) {
-			
+
 			if (plugin.CommandExecutor.isSpectating.get(event.getPlayer())) {
-				
+
 				event.setCancelled(true);
-				
+
 			}
-			
+
 		}
-		
+
 	}
-	
+
 	@EventHandler
 	public void onBlockPlace(BlockPlaceEvent event) {
-		
+
 		if (plugin.CommandExecutor.isSpectating.get(event.getPlayer()) != null) {
-			
+
 			if (plugin.CommandExecutor.isSpectating.get(event.getPlayer())) {
-			
+
 				event.setCancelled(true);
-			
+
 			}
-			
+
 		}
-		
+
 	}
-	
+
 	@EventHandler
 	public void onBlockBreak(BlockBreakEvent event) {
-		
+
 		if (plugin.CommandExecutor.isSpectating.get(event.getPlayer()) != null) {
-			
+
 			if (plugin.CommandExecutor.isSpectating.get(event.getPlayer())) {
-			
+
 				event.setCancelled(true);
-			
+
 			}
-			
+
 		}
-		
+
 	}
-	
+
 	@EventHandler
 	public void onEnitityDamage(EntityDamageEvent event) {
-		
+
 		if (event instanceof Player) {
-			
+
 			Player pla = (Player)event.getEntity();
-			
+
 			if (plugin.CommandExecutor.isSpectating.get(pla) != null) {
-				
+
 				if (plugin.CommandExecutor.isSpectating.get(pla)) {
-			
+
 					event.setCancelled(true);
-			
+
 				}
 			}
 		}
@@ -192,35 +219,38 @@ public class SpectateListener implements Listener {
 
 	@EventHandler
 	public void onEntityDeath(EntityDeathEvent event) {
-		
-		    if (event instanceof PlayerDeathEvent) {
-		    	
-		    	Player pla = (Player)event.getEntity();
-		    	
-					if (plugin.CommandExecutor.isBeingSpectated.get(pla) != null) {
-						
-						if (plugin.CommandExecutor.isBeingSpectated.get(pla)) {
-							
-							plugin.CommandExecutor.spectator.get(pla).sendMessage("§7You were forced to stop spectating because the person who you were spectating died.");
-							
-							plugin.CommandExecutor.spectator.get(pla).teleport(plugin.CommandExecutor.origLocation.get(plugin.CommandExecutor.spectator.get(pla)));
-							plugin.CommandExecutor.isSpectating.put(plugin.CommandExecutor.spectator.get(pla), false);
-							plugin.CommandExecutor.isBeingSpectated.put(pla, false);
-							
-							for (Player playp : plugin.CommandExecutor.spectator.get(pla).getWorld().getPlayers()) {
-							
-								plugin.visible(plugin.CommandExecutor.spectator.get(pla), playp);
-								
-							}
-							
-							plugin.getNative(plugin.CommandExecutor.spectator.get(pla)).netServerHandler.sendPacket(new Packet29DestroyEntity(plugin.CommandExecutor.spectator.get(pla).getEntityId()));
-							plugin.visible(plugin.CommandExecutor.target.get(plugin.CommandExecutor.spectator.get(pla)), plugin.CommandExecutor.spectator.get(pla));
-							
-						}
-						
+
+		if (event instanceof PlayerDeathEvent) {
+
+			Player pla = (Player)event.getEntity();
+
+			if (plugin.CommandExecutor.isBeingSpectated.get(pla) != null) {
+
+				if (plugin.CommandExecutor.isBeingSpectated.get(pla)) {
+
+					plugin.CommandExecutor.spectator.get(pla).sendMessage("§7You were forced to stop spectating because the person who you were spectating died.");
+
+					plugin.CommandExecutor.spectator.get(pla).teleport(plugin.CommandExecutor.origLocation.get(plugin.CommandExecutor.spectator.get(pla)));
+					plugin.CommandExecutor.isSpectating.put(plugin.CommandExecutor.spectator.get(pla), false);
+					plugin.CommandExecutor.isBeingSpectated.put(pla, false);
+					plugin.CommandExecutor.spectator.get(pla).getInventory().clear();
+					plugin.CommandExecutor.spectator.get(pla).getInventory().setContents(plugin.CommandExecutor.senderInv.get(plugin.CommandExecutor.spectator.get(pla)));
+					plugin.CommandExecutor.spectator.get(pla).getInventory().setArmorContents(plugin.CommandExecutor.senderArm.get(plugin.CommandExecutor.spectator.get(pla)));
+					plugin.CommandExecutor.spectator.get(pla).setHealth(plugin.CommandExecutor.senderHealth.get(plugin.CommandExecutor.spectator.get(pla)));
+					plugin.CommandExecutor.spectator.get(pla).setFoodLevel(plugin.CommandExecutor.senderHunger.get(plugin.CommandExecutor.spectator.get(pla)));
+
+					for (Player playp : plugin.CommandExecutor.spectator.get(pla).getWorld().getPlayers()) {
+
+						plugin.CommandExecutor.spectator.get(pla).showPlayer(playp);
+
 					}
-		    }
-	
+
+				}
+
+			}
+
+		}
+
 	}
-	
+
 }
